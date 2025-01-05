6968ad497bb7cda80e8c10a8f8ae91d1b224ad79
ReGeLePuMa <scpetrea@gmail.com>
2025-01-05 21:49:31 +0200

    Additional Grafana config

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
 docker swarm leave --force 2> /dev/null
-docker swarm init 2> /dev/null
-
 docker compose -f stack.yml build
+docker swarm init 2> /dev/null
 docker stack deploy -c stack.yml tema3 --detach=false
