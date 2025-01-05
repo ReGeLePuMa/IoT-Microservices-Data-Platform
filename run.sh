1e82fc6526c2bcc73b5490661826234caec3d005
ReGeLePuMa <scpetrea@gmail.com>
2025-01-05 23:45:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker swarm leave --force 2> /dev/null
 docker compose -f stack.yml build
 docker swarm init 2> /dev/null
-docker stack deploy -c stack.yml tema3 --detach=false
+docker stack deploy -c stack.yml scd3 --detach=false
