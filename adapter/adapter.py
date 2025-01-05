3668b9f01e6c437ba2a0d29f4376c140e7c57d5d
ReGeLePuMa <scpetrea@gmail.com>
2025-01-05 03:22:01 +0200

    Fixed InfluxDB connectivity issue

--- adapter/adapter.py
+++ adapter/adapter.py
@@ -3,13 +3,20 @@ import json
 import os
 from logger import logger
 from datetime import datetime
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 
+db_url = os.getenv("INFLUXDB_URL", "http://localhost:8086")
+db_token = os.getenv("INFLUXDB_TOKEN", "my-token")
+db_org = os.getenv("INFLUXDB_ORG", "my-org")
 
 influx_client = InfluxDBClient(
-    host=os.getenv("INFLUXDB_HOST", "localhost"),
-    port=int(os.getenv("INFLUXDB_PORT", "8086")),
+    url=db_url,
+    token=db_token,
+    org=db_org
 )
+
+write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
 class MQTTChatClient:
     def __init__(self, broker_address, broker_port, topic_subscribe):
@@ -32,13 +39,14 @@ class MQTTChatClient:
         data = json.loads(msg.payload)
         logger.info(f"Received a message by topic [{topic}]")
 
-        timestamp = data.get("timestamp", datetime.now().strftime('%Y-%M-%d %H:%M:%S'))
+        timestamp = data.get("timestamp", datetime.now().isoformat())
         logger.info(f"Data timestamp is{f': {timestamp}' if data.get('timestamp') else ' NOW'}")
 
         location, station = topic.split("/")
         for key, value in data.items():
             if isinstance(value, (int, float)):
-                influx_client.write_points([
+                write_api.write("tema3", db_org, 
+                [
                     {
                         "measurement": f"{location}.{station}.{key}",
                         "tags": {
@@ -52,6 +60,7 @@ class MQTTChatClient:
                     }
                 ])
                 logger.info(f"{location}.{station}.{key} {value}")
+        logger.info("")
 
     def connect(self):
         self.client.connect(self.broker_address, self.broker_port)
@@ -67,7 +76,6 @@ class MQTTChatClient:
 if __name__ == "__main__":
     broker_address = os.getenv("BROKER_HOST", "localhost")
     broker_port = int(os.getenv("BROKER_PORT", "1883"))
-    influx_client.switch_database("tema3")
 
     chat_client = MQTTChatClient(broker_address, broker_port, "#")
     chat_client.connect()
